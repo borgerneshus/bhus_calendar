d47ed582202a0b2cf321bab384ce24cc83aedc89
Tobias F. Petersen <tobsf@hotmail.com>
2018-01-17 11:38:31 +0100

    Update feature

--- js/oc_bhus_calendar.js
+++ js/oc_bhus_calendar.js
@@ -8,6 +8,6 @@ jQuery( document ).ready(function($) {
     $('option:contains("Amfitrappen")').addClass('colors-taxonomy-term-Amfitrappen');
     $('option:contains("Borgernes Torv")').addClass('colors-taxonomy-term-Borgernes_værksted');
     $('option:contains("Borgernes værksted")').addClass('colors-taxonomy-term-Borgernes_Torv');
-   
+    $('label[for=edit-field-date-value-min]').toggle();
 });
 
