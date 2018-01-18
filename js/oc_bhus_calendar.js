419c5a229a6d6a1737f5346550e26cf7931dde03
Tobias F. Petersen <tobsf@hotmail.com>
2018-01-18 14:40:41 +0100

    update colors

--- js/oc_bhus_calendar.js
+++ js/oc_bhus_calendar.js
@@ -6,8 +6,8 @@ jQuery( document ).ready(function($) {
      * Create options color
      */
     $('option:contains("Amfitrappen")').addClass('colors-taxonomy-term-Amfitrappen');
-    $('option:contains("Borgernes Torv")').addClass('colors-taxonomy-term-Borgernes_værksted');
-    $('option:contains("Borgernes værksted")').addClass('colors-taxonomy-term-Borgernes_Torv');
+    $('option:contains("Borgernes Torv")').addClass('colors-taxonomy-term-Borgernes_Torv');
+    $('option:contains("Borgernes værksted")').addClass('colors-taxonomy-term-Borgernes_værksted');
     $('label[for=edit-field-date-value-min]').toggle();
 });
 
