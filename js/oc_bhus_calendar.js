15282acdd24c60397613fc3ea9740ddb9408afdb
Tobias F. Petersen <tobsf@hotmail.com>
2018-01-24 15:00:57 +0100

    colors

--- js/oc_bhus_calendar.js
+++ js/oc_bhus_calendar.js
@@ -10,7 +10,7 @@ jQuery( document ).ready(function($) {
     $('option:contains("Borgernes værksted")').addClass('colors-taxonomy-term-Borgernes_værksted');
     $('option:contains("Gardinrum")').addClass('colors-taxonomy-term-Gardinrum');
     $('option:contains("Musikscenen")').addClass('colors-taxonomy-term-Musikscenen');
-    $('option:contains("Scene)').addClass('colors-taxonomy-term-Scene');
+    $('option:contains("Scene")').addClass('colors-taxonomy-term-Scene');
     $('option:contains("Store scene")').addClass('colors-taxonomy-term-Store_scene');
     $('option:contains("Udstillingsområde kælder")').addClass('colors-taxonomy-term-Udstillingsområde_kælder');
     $('option:contains("Udstillingsområde stue")').addClass('colors-taxonomy-term-Udstillingsområde_stue');
