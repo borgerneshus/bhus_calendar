800f2f9e282a13b375cf47402c787d52d00cf7cd
Tobias F. Petersen <tobsf@hotmail.com>
2018-01-24 13:40:18 +0100

    remove spelling error

--- templates/calendar-week-overlap.tpl.php
+++ templates/calendar-week-overlap.tpl.php
@@ -198,7 +198,7 @@ $is_full_view = variable_get("bhus_calendar_full_view", false);
         <div class="modal-content">
             <div class="modal-header">
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-                <h4 class="modal-title">Arrangemants informationer</h4>
+                <h4 class="modal-title">Informationer</h4>
             </div>
             <div class="modal-body">
                 <p>One fine body&hellip;</p>
