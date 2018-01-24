882774d68e8a1a36c8694ea8b959abcfd652c938
Tobias F. Petersen <tobsf@hotmail.com>
2018-01-24 14:59:23 +0100

    js add colors to options

--- js/oc_bhus_calendar.js
+++ js/oc_bhus_calendar.js
@@ -8,6 +8,12 @@ jQuery( document ).ready(function($) {
     $('option:contains("Amfitrappen")').addClass('colors-taxonomy-term-Amfitrappen');
     $('option:contains("Borgernes Torv")').addClass('colors-taxonomy-term-Borgernes_Torv');
     $('option:contains("Borgernes værksted")').addClass('colors-taxonomy-term-Borgernes_værksted');
+    $('option:contains("Gardinrum")').addClass('colors-taxonomy-term-Gardinrum');
+    $('option:contains("Musikscenen")').addClass('colors-taxonomy-term-Musikscenen');
+    $('option:contains("Scene)').addClass('colors-taxonomy-term-Scene');
+    $('option:contains("Store scene")').addClass('colors-taxonomy-term-Store_scene');
+    $('option:contains("Udstillingsområde kælder")').addClass('colors-taxonomy-term-Udstillingsområde_kælder');
+    $('option:contains("Udstillingsområde stue")').addClass('colors-taxonomy-term-Udstillingsområde_stue');
     $('label[for=edit-field-date-value-min]').toggle();
 });
 
